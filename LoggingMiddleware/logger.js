1acf679ad1f1251059157e50838249d8606cd568
Jhansi Lakshmi Sambhana <jhansilakshmisambhana12@gmail.com>
2026-06-23 12:51:24 +0530

    Add files via upload

---
+++ LoggingMiddleware/logger.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const Log = async (
+  stack,
+  level,
+  packageName,
+  message,
+  token
+) => {
+  try {
+    await axios.post(
+      "http://4.224.186.213/evaluation-service/logs",
+      {
+        stack,
+        level,
+        package: packageName,
+        message
+      },
+      {
+        headers: {
+          Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzYW1iaGFuYWpoYW5zaWxha3NobWkuMjMuY3NtQGFuaXRzLmVkdS5pbiIsImV4cCI6MTc4MjE5ODA5NSwiaWF0IjoxNzgyMTk3MTk1LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiZmI3ZjRhMGUtODIzYy00NGU1LTlkMzItODk4NTI5MWQ2MTJiIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoic2FtYmhhbmEgamhhbnNpIGxha3NobWkiLCJzdWIiOiJjOGIyYzk3NS05M2ZjLTQzNjItYmNkMi1lZDVmZmNkNTFiZmUifSwiZW1haWwiOiJzYW1iaGFuYWpoYW5zaWxha3NobWkuMjMuY3NtQGFuaXRzLmVkdS5pbiIsIm5hbWUiOiJzYW1iaGFuYSBqaGFuc2kgbGFrc2htaSIsInJvbGxObyI6ImEyMzEyNjU1MjA0OCIsImFjY2Vzc0NvZGUiOiJNVHF4YXIiLCJjbGllbnRJRCI6ImM4YjJjOTc1LTkzZmMtNDM2Mi1iY2QyLWVkNWZmY2Q1MWJmZSIsImNsaWVudFNlY3JldCI6Imh0bWVxQWJ0d2dha1hHV1oifQ.wOcxxQ7wPWT8OB3hNOuJmPs7rFXRqQNl0bPmBXsIG9c",
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  } catch (error) {
+    console.error("Logging failed:", error.message);
+  }
+};
